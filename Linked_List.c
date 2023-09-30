997f8af2dc31c50c0ae789db1d61500f788eeafb
Md Mehedi Hasan Rony <125382191+r0nY-0017@users.noreply.github.com>
2023-09-30 08:32:36 +0600

    Linked_List.c

--- Linked_List.c
+++ Linked_List.c
@@ -2,46 +2,49 @@
 #include <string.h>
 #include <stdlib.h>
 
-struct hotel{
-    char name[20];
-    int room_no;
-    char arrival_date[10];
-    char departure_date[10];
-    struct hotel *next;
+struct EmployeeNode{
+    int EmployeeID,age;
+    char EmployeeName[20];
+    float salary;
+    struct EmployeeNode *back;
+    struct EmployeeNode *next;
 };
 
-void Create_List(struct hotel *p){
-    int n,room;
-    char name[20], d1[10], d2[10];
-    printf("Enter Number of Customer : ");
-    scanf("%d", &n); //number of customer
-    for(int i=1; i<=n; i++){
-        printf("Enter Customer-%d data: ", i);
-        scanf("%s %d %s %s", name, &room, d1, d2);
-        strcpy(p->name, name);
-        p->room_no = room;
-        strcpy(p->arrival_date ,d1);
-        strcpy(p->departure_date, d2);
+void print(struct EmployeeNode *p){
+    while (p != NULL){
+        printf("%d %s %d %.2f\n", p->EmployeeID, p->EmployeeName, p->age, p->salary);
+        p = p->back;
+    }
+}
 
-        if(i<n){
-            struct hotel *temp = malloc(sizeof(struct hotel));
+void create_List(struct EmployeeNode *p){
+    p->back = NULL;
+    int n, id, age;
+    char nm[20];
+    float sal;
+    printf("Enter Number of Employee : ");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++){
+        printf("Enter Employee-%d data: ", i);
+        scanf("%d %s %d %f", &id, nm, &age, &sal);
+        p->EmployeeID = id;
+        strcpy(p->EmployeeName, nm);
+        p->age = age;
+        p->salary = sal;
+        
+        if (i < n){
+            struct EmployeeNode *temp = malloc(sizeof(struct EmployeeNode));
             p->next = temp;
+            temp->back = p;
             p = p->next;
         }
     }
     p->next = NULL;
-}
-
-void print_List(struct hotel *p){
-    printf("\nYour Linked List are :\n");
-    while(p != NULL){
-        printf("%s %d %s %s\n", p->name, p->room_no, p->arrival_date, p->departure_date);
-        p = p->next;
-    }
+    print(p);
 }
 
 int main(){
-    struct hotel *head = malloc(sizeof(struct hotel));
-    Create_List(head);
-    print_List(head);
+    struct EmployeeNode *head = malloc(sizeof(struct EmployeeNode));
+    create_List(head);
+return 0;
 }
